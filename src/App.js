fc71785c52a5e488353f05829c78e4128ced423b
vbouault <vbouault@hotmail.fr>
2020-06-29 15:59:47 +0200

    authentification back-office

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import './App.css';
+import Login from './components/Login';
+import AuthContext from './authContext';
+import jwtDecode from 'jwt-decode';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import PrivateRoute from './components/PrivateRoute';
+import Register from './components/Register';
 
 function App () {
+  const [token, setToken] = useState(localStorage.getItem('authToken'))
+  const setTokenInLocalStorage = (token) => {
+    localStorage.setItem('authToken', token)
+    setToken(token)
+  }
+
+  let userNameFromToken = null
+  if (token) {
+    userNameFromToken = jwtDecode(token).name || null
+  }
+
+
   return (
-    <div className='App'>
-      <h1>Valiris Résidence</h1>
-    </div>
+    <AuthContext.Provider value={{token, setToken: setTokenInLocalStorage}}>
+      <Router>
+        <div className="App">
+        </div>
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <PrivateRoute path="/register">
+            <Register />
+          </PrivateRoute>
+          <PrivateRoute path="/">
+            <div>
+              pag d'acceuil
+            </div>
+          </PrivateRoute>
+        </Switch>
+      </Router>
+    </AuthContext.Provider>
   );
 }
 
