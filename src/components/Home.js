c51206c91843d68ef489a370fdbadc5ca3878b41
Nathan Guillaumin <nathan.guillaumin@outlook.com>
2020-06-30 15:08:42 +0200

    adds map on bookings

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import BookingCard from './BookingCard';
+import API from '../API';
 
 const Home = () => {
+    const [bookings, setBookings] = useState();
+
+    useEffect(() => {
+        API.get('/bookings')
+          .then(res => res.data)
+          .then(data => setBookings(data.map(b => {
+              return { 
+                firstname: b.firstname,
+                lastname: b.lastname,
+                starting_date: b.starting_date,
+                ending_date: b.ending_date,
+                message: b.content
+            }
+          })))
+      }, []);
+
+    if (!bookings) {
+    return <p>loading...</p>;
+    } else {
     return (
         <div>
-            <BookingCard />
+            {bookings.map(b => <BookingCard />)}
         </div>
     );
+  }      
 };
 
 export default Home;
