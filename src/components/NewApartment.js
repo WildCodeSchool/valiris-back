e707c639944104cbd176bf56338299184212a1c9
K0Si-003 <hugo.pioda@gmail.com>
2020-07-02 18:04:13 +0200

    clean console.log

--- src/components/NewApartment.js
+++ src/components/NewApartment.js
@@ -36,7 +36,6 @@ const NewApartment = (props) => {
     formData.append('week_price', apartment.weekPrice);
     formData.append('month_price', apartment.monthPrice);
     formData.append('main_picture_url', mainPicture);
-    console.log(formData)
     API.post('/apartments', formData, {
       headers: {
         'Content-Type': 'multipart/form-data' 
@@ -44,7 +43,6 @@ const NewApartment = (props) => {
     })
       .then(res => res.data)
       .then(data => {
-        console.log(data)
         setMessageForm(true);
         setLoading(false);
         setMsgAlert(`L'appartement ${data.name} a bien été créé`);
