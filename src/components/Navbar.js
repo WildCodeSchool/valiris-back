1d461241fb2868762f75d66d63fcca1df21207dd
vbouault <vbouault@hotmail.fr>
2020-07-05 10:56:14 +0200

    deconnexion

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,29 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
 import '../styles/navbar.css';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
+import AuthContext from '../authContext';
 
 const Navbar = () => {
 
-return (
-<AppBar position="static">
-  <Toolbar>
-    <Typography variant="h6" className='navbar-list'>
-        <Button color="inherit"><NavLink className='navbar-item' to='/'>Accueil</NavLink></Button>
-        <Button color="inherit"><NavLink className='navbar-item' to='/appartements'>Appartements</NavLink></Button>
-        <Button color="inherit"><NavLink className='navbar-item' to='/contacts'>Contacts</NavLink></Button>
-        <Button color="inherit"><NavLink className='navbar-item' to='/calendrier'>Calendrier</NavLink></Button>
-    </Typography>
-    <Button color="inherit" >Deconnexion</Button>
-  </Toolbar>
-</AppBar>
-)
+  const { setToken: setTokenInLocalStorage } = useContext(AuthContext);
+
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" className='navbar-list'>
+          <Button color="inherit"><NavLink className='navbar-item' to='/'>Accueil</NavLink></Button>
+          <Button color="inherit"><NavLink className='navbar-item' to='/appartements'>Appartements</NavLink></Button>
+          <Button color="inherit"><NavLink className='navbar-item' to='/contacts'>Contacts</NavLink></Button>
+          <Button color="inherit"><NavLink className='navbar-item' to='/calendrier'>Calendrier</NavLink></Button>
+        </Typography>
+        <Button color="inherit" onClick={() => setTokenInLocalStorage('')}>Deconnexion</Button>
+      </Toolbar>
+    </AppBar>
+  )
 };
 
 export default Navbar;
