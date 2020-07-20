7adffecf6e0b7d5dcc3de0746df972621ec97921
Nathan Guillaumin <nathan.guillaumin@outlook.com>
2020-07-20 17:01:03 +0200

    adds message error for submitting contact with same e-mail address

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -65,8 +65,13 @@ const Contact = (props) => {
           setMsgAlert(`Le contact ${data.firstname} ${data.lastname} a bien été mis à jour.`);
         })
         .catch(err => {
-          console.log(err);
-          setMsgAlert('Une erreur est survenue, veuillez essayer à nouveau.');
+          const errorMessage = err.response.data.errorMessage;
+          console.log(errorMessage)
+          if (errorMessage === 'Email already exists') {
+            setMsgAlert('Cet e-mail existe déjà dans votre liste de contacts.');
+          } else {
+            setMsgAlert('Une erreur est survenue, veuillez essayer à nouveau.');
+          }
           setErrorForm(true);
           setLoading(false);
           setMessageForm(true);
